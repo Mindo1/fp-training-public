d6764aa04e58ab5a08b3ff3f74c955421d42880a
Matchima Klomkasem <88373747+Mindo1@users.noreply.github.com>
2026-05-01 14:49:20 +0700

    Add tests for phone number normalization and international conversion

--- code/src/03-Currying/begin/currying.test.ts
+++ code/src/03-Currying/begin/currying.test.ts
@@ -1,8 +1,16 @@
-import { convertToInternational, normalizePhoneNumber } from "./currying"
+import { describe, it, expect } from '@jest/globals'
+import { convertToInternational, normalizePhoneNumber, toInternationalPhoneNumber } from "./currying"
+import { pipe } from 'fp-ts/lib/function'
 
 describe('Currying', () => {
   it('should normalize phone number', () => {
     expect(normalizePhoneNumber('01012345678')).toBe('1012345678')
     expect(convertToInternational('66', '1012345678')).toBe('+661012345678')
+  })
+
+  it('should convert phone number to international format', ()=>{
+    const forThailand = '66';
+
+    expect(pipe('01012345678', normalizePhoneNumber, toInternationalPhoneNumber(forThailand))).toEqual("+661012345678")
   })
 })
--- code/src/03-Currying/begin/currying.ts
+++ code/src/03-Currying/begin/currying.ts
@@ -3,3 +3,7 @@ export const normalizePhoneNumber = (s: string) =>
 
 export const convertToInternational = (countryCode: string, digits: string) =>
   `+${countryCode}${digits}`
+
+type ToPhoneNumber = (countryCode: string) => (digits: string) => string;
+
+export const toInternationalPhoneNumber: ToPhoneNumber = (countryCode) => (digits) => `+${countryCode}${digits}`
