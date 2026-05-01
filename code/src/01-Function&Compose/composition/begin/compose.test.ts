4765d5d1fcacece5ab932b19a0eb3330d1c95c9d
Matchima Klomkasem <88373747+Mindo1@users.noreply.github.com>
2026-05-01 14:49:28 +0700

    Refactor compose and add tests for incrementThenToString functionality

--- code/src/01-Function&Compose/composition/begin/compose.test.ts
+++ code/src/01-Function&Compose/composition/begin/compose.test.ts
@@ -1,20 +1,43 @@
+import { describe, it, expect } from "@jest/globals";
 import {
   toString,
   increment,
-  // incrementThenToString, 
-  // compose 
-} from "./compose"
+  incrementThenToString,
+  compose,
+  add,
+  // incrementThenToString,
+  // compose
+} from "./compose";
 
-describe('Composition', () => {
-  it('should compose two functions', () => {
-    expect(toString(increment(1))).toBe('2')
+describe("Composition", () => {
+  it("should compose two functions", () => {
+    expect(toString(increment(1))).toBe("2");
+  });
+
+  it("should compose two functions with incrementThenToString", () => {
+    expect(incrementThenToString(1)).toBe("2");
+  });
+
+  it("should compose two functions with my own compose", () => {
+    expect(compose(increment, toString)(1)).toBe("2");
+  });
+});
+
+describe("Add", () => {
+  it("should add two numbers", () => {
+    expect(add(1)(2)).toBe(3);
+  });
+
+  it('should add with toString compose', () => {
+    const add1 = add(1);
+    const add1TheToString = compose(add1, toString);
+
+    expect(add1TheToString(2)).toBe("3");
   })
 
-  it('should compose two functions with incrementThenToString', () => {
-    // expect(incrementThenToString(1)).toBe('2')
+  it('should add with incrementThenToString compose', () => {
+    const add1 = add(1);
+    const incrementThenToStringAdd1 = compose(add1, incrementThenToString);
+    expect(incrementThenToStringAdd1(2)).toBe("4");
   })
-
-  it('should compose two functions with my own compose', () => {
-    // expect(compose(increment, toString)(1)).toBe('2')
-  })
-})
+});
--- code/src/01-Function&Compose/composition/begin/compose.ts
+++ code/src/01-Function&Compose/composition/begin/compose.ts
@@ -1,7 +1,27 @@
-type Increment = (x: number) => number
-export const increment: Increment = x => x + 1
+type Increment = (x: number) => number;
+export const increment: Increment = (x) => x + 1;
 
-type ToString = (x: number) => string
-export const toString: ToString = x => `${x}`
+type ToString = (x: number) => string;
+export const toString: ToString = (x) => `${x}`;
 
 // Start here...
+type IncrementThenToString = (x: number) => string;
+
+export const incrementThenToString: IncrementThenToString = (x) =>
+  toString(increment(x));
+
+// export const compose =
+//   (increment: Increment, toString: ToString) => (x: number) =>
+//     toString(increment(x));
+
+// type Compose = (increment: Increment, toString: ToString) => (x:number) => string
+
+// export const compose: Compose = (increment, toString)=> x => toString(increment(x))
+
+type Compose = <N, S>(f: (y: N) => N, g: (x: N) => S) => (x: N) => S;
+export const compose: Compose = (f, g) => (x) => g(f(x));
+
+
+type Add = (x:number)=> (y:number)=> number;
+export const add: Add = x => y => x + y;
+
